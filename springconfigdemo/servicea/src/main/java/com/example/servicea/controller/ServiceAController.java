1d805f66b864e317a0ebae2a6aa7a5f6e4ab3588
ruanxingke <rxingke@gmail.com>
2020-08-21 21:23:15 -0500

    add spring config demo

---
+++ springconfigdemo/servicea/src/main/java/com/example/servicea/controller/ServiceAController.java
@@ -0,0 +1,16 @@
+package com.example.servicea.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceAController {
+    @Value("${greeting}")
+    private String message;
+
+    @RequestMapping("/")
+    public String getName(){
+        return message;
+    }
+}
---
+++ springconfigdemo/serviceb/src/main/java/com/example/serviceb/controller/ServiceBController.java
@@ -0,0 +1,16 @@
+package com.example.serviceb.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceBController {
+    @Value("${greeting}")
+    private String message;
+
+    @RequestMapping("/")
+    public String getName(){
+        return message;
+    }
+}
